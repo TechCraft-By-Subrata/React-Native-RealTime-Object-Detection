732d88b1d3910632b4b74e4e61aa93928ce2dbf5
Subrata <codingsubrata@gmail.com>
2026-04-23 23:02:11 +0530

    .

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { initExecutorch } from 'react-native-executorch';
+import { BareResourceFetcher } from 'react-native-executorch-bare-resource-fetcher';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Initialize ExecuTorch with the bare resource fetcher
+initExecutorch({ resourceFetcher: BareResourceFetcher });
 
 AppRegistry.registerComponent(appName, () => App);
